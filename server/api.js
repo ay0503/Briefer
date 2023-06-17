a2bfc5ebc5b023fcbe46ef9ba5f99b77fcabb35d
Andrew Youn <ayoun@atlassian.com>
2023-06-17 15:55:01 -0700

    added api.js skeleton to server

--- server/api.js
+++ server/api.js
@@ -1,2 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
 
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+app.post('/summarize', (req, res) => {
+  const text = req.body.text;
+
+  // Here you would typically call a function to summarize the text.
+  // For simplicity, we'll just return the first 100 characters.
+  const summary = text.substring(0, 100);
+
+  res.send(summary);
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
